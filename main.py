bd7b2e37048fb5d5b8ddbe9a6c033e21589f6f06
Jared De Blander <jared@ayrscott.com>
2023-06-27 07:43:51 -0400

    simple error handling

--- main.py
+++ main.py
@@ -5,52 +5,55 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"error": "Hello World"}
 
 @app.get("/roll/{dice}")
 async def roll(dice: str):
-    results = {}
-    results['rolls'] = []
-    results['modifier'] = 0
-    results['total'] = 0
-    dice = dice.lower()
+    try:
 
-    # split off a + or - modifier if it exists
+        results = {}
+        results['rolls'] = []
+        results['modifier'] = 0
+        results['total'] = 0
+        dice = dice.lower()
 
-    if "+" in dice:
-        dice, modifier = dice.split("+")
-        results['modifier'] = int(modifier)
-    elif "-" in dice:
-        dice, modifier = dice.split("-")
-        results['modifier'] = -int(modifier)
+        # split off a + or - modifier if it exists
 
-    # split off the number of dice and the number of sides
-    num_dice, num_sides = dice.split("d")
-    if len(num_dice) == 0:
-        num_dice = 1
+        if "+" in dice:
+            dice, modifier = dice.split("+")
+            results['modifier'] = int(modifier)
+        elif "-" in dice:
+            dice, modifier = dice.split("-")
+            results['modifier'] = -int(modifier)
 
-    num_dice = int(num_dice)
+        # split off the number of dice and the number of sides
+        num_dice, num_sides = dice.split("d")
+        if len(num_dice) == 0:
+            num_dice = 1
 
-    if num_dice > 100:
-        return {"message": "Too many dice!"}
+        num_dice = int(num_dice)
 
-    if num_dice < 1:
-        return {"message": "Too few dice!"}
+        if num_dice > 100:
+            return {"error": "Too many dice!"}
 
-    num_sides = int(num_sides)
+        if num_dice < 1:
+            return {"error": "Too few dice!"}
 
-    # roll each die, add the roll to the rolls array
-    for _ in range(num_dice):
-        roll = random.randint(1, num_sides)
-        results['rolls'].append(roll)
-        results['total'] += roll
+        num_sides = int(num_sides)
 
-    # add the modifier
-    results['total'] += results['modifier']
+        # roll each die, add the roll to the rolls array
+        for _ in range(num_dice):
+            roll = random.randint(1, num_sides)
+            results['rolls'].append(roll)
+            results['total'] += roll
 
-    return results
+        # add the modifier
+        results['total'] += results['modifier']
 
+        return results
 
+    except Exception as e:
+        return {"error": "Something went wrong!"}
 
 
 
